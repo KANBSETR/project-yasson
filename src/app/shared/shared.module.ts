4e5f08574a7f22f793aaff1f246bd2a766684078
Nicolás Díaz <azobisiso576@gmail.com>
2024-08-25 20:43:50 -0400

    Pocos cambios

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -3,13 +3,14 @@ import { CommonModule } from '@angular/common';
 import { IonicModule } from '@ionic/angular';
 import {MenuAdminComponent} from '../components/menu-admin/menu-admin.component';
 import { RouterModule } from '@angular/router';
+import { ProductsComponent } from '../components/products/products.component';
 @NgModule({
-  declarations: [MenuAdminComponent],
+  declarations: [MenuAdminComponent, ProductsComponent], //Declarar el componente que se quiere compartir
   imports: [
     CommonModule,
     IonicModule,
     RouterModule,
   ],
-  exports: [MenuAdminComponent, IonicModule, RouterModule]
+  exports: [MenuAdminComponent, ProductsComponent] //Exportar el componente que se quiere compartir
 })
 export class SharedModule {}
