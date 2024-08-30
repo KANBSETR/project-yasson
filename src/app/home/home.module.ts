7a2d60fc1ab78de373651868a5f1c72fe4848074
Benjamin1275 <b.gonzalez5@duocuc.cl>
2024-08-30 01:33:15 -0400

    Se agrego el home, se cambio un poco el login y el registro, se agregaron flechas para retroceder y algunas bibliotecas que no ocupe

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,11 +1,8 @@
-import { NgModule } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-
 import { IonicModule } from '@ionic/angular';
-
 import { HomePageRoutingModule } from './home-routing.module';
-
 import { HomePage } from './home.page';
 import { SharedModule } from '../shared/shared.module';
 
@@ -17,6 +14,7 @@ import { SharedModule } from '../shared/shared.module';
     HomePageRoutingModule,
     SharedModule
   ],
-  declarations: [HomePage]
+  declarations: [HomePage],
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
 export class HomePageModule {}
